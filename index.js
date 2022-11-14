09414b42086b77ce09a7787471a8f4f5fd35f0e4
M Newman <megan.m.boggs@gmail.com>
2022-11-13 22:07:32 -0800

    update js to render various posts

--- index.js
+++ index.js
@@ -29,3 +29,36 @@ const posts = [
   },
 ];
 
+const mainEl = document.getElementById("main");
+const likesEl = document.getElementById("likes");
+
+appendMain();
+
+function appendMain() {
+  for (let i = 0; i < posts.length; i++) {
+    const htmlStructure = `
+            <section>
+                    <div class="container">
+                        <div class="user-info">
+                            <img src="${posts[i].avatar}" alt="Avatar of ${posts[i].name}">
+                            <div class="user-info-p">
+                                <p class="user-name">${posts[i].name}</p>
+                                <p class="user-location">${posts[i].location}</p>
+                            </div>
+                        </div>
+    
+                        <img class="image-post likes" src="${posts[i].post}" alt="Picture posted by ${posts[i].name}">
+    
+                        <div class="interaction-icons">
+                            <img class="like-btn"src="images/icon-heart.png" alt="Like button">
+                            <img src="images/icon-comment.png" alt="Comment button">
+                            <img src="images/icon-dm.png" alt="Direct message button">
+                        </div>
+                        <p class="likes" id="likes">${posts[i].likes} likes</p>
+                        <p class="desc"><span class="desc-span-nick">${posts[i].username}</span> ${posts[i].comment}</p>
+                    </div>
+                </section>
+        `;
+    mainEl.innerHTML += htmlStructure;
+  }
+}
